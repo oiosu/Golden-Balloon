b4d8c7d94d9c1824ebefe2fff71b127edf3acb00
ererink <yelkim0210@gmail.com>
2022-11-01 15:22:27 +0900

    accounts/mypages, follow

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('<int:pk>/', views.mypage_1, name='mypage_1'),
-    path('<int:pk>/', views.mypage_2, name='mypage_2'),
+    path('mypage_1/<int:pk>/', views.mypage_1, name='mypage_1'),
+    path('update/', views.update, name='update'),
+    path('mypage_2/<int:pk>/', views.mypage_2, name='mypage_2'),
+    path('<int:pk>/follow/', views.follow, name='follow'),
 ]
