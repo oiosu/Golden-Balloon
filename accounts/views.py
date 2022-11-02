6e3b0dc65ee8207f0b840177cc92d334ac0bbf8a
soohofather <enthchal@gmail.com>
2022-11-02 14:26:10 +0900

    동현:리뷰페이지 중간저장

--- accounts/views.py
+++ accounts/views.py
@@ -13,90 +13,98 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('articles:index')
+            return redirect("articles:index")
     else:
         form = CustomUserCreationForm()
-    
+
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'accounts/signup.html', context)
+    return render(request, "accounts/signup.html", context)
+
 
 def login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        user = auth.authenticate(request, email=email, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('articles:index')
-        else:
-            return render(request, 'accounts/login.html', {'error': '이메일 혹은 비밀번호가 일치하지 않습니다.'})
-    
+    if request.method == "POST":
+        #     email = request.POST["email"]
+        #     password = request.POST["password"]
+        #     user = auth.authenticate(request, email=email, password=password)
+        #     if user is not None:
+        #         auth.login(request, user)
+        #         return redirect("articles:index")
+        #     else:
+        #         return render(
+        #             request, "accounts/login.html", {"error": "이메일 혹은 비밀번호가 일치하지 않습니다."}
+        #         )
+
+        # else:
+        #     return render(request, "accounts/login.html")
+        #     return render(request)
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get("next") or "articles:index")
+
     else:
-        return render(request, 'accounts/login.html')
-    #     return render(request)
-    #     form = AuthenticationForm(request, data=request.POST)
-    #     if form.is_valid():
-    #         auth_login(request, form.get_user())
-    #         return redirect(request.GET.get('next') or 'articles:index')
+        form = AuthenticationForm()
 
-    # else:
-    #     form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
 
-    # context = {
-    #     'form': form,
-    # }
-    # return render(request, 'accounts/login.html', context)
 
 def logout(request):
     auth_logout(request)
-    return redirect('articles:index')
+    return redirect("articles:index")
+
 
 def mypage_1(request, pk):
     user = get_user_model().objects.get(pk=pk)
 
     context = {
-        'user': user,
+        "user": user,
     }
-    return render(request, 'accounts/mypage_1.html', context)
+    return render(request, "accounts/mypage_1.html", context)
+
 
 def mypage_2(request, pk):
     user = get_user_model().objects.get(pk=pk)
 
     context = {
-        'user': user,
+        "user": user,
     }
-    return render(request, 'accounts/mypage_2.html', context)
+    return render(request, "accounts/mypage_2.html", context)
+
 
 @login_required
 def follow(request, pk):
     user = get_object_or_404(get_user_model(), pk=pk)
     if request.user == user:
-        messages.warning(request, '스스로 팔로우 할 수 없습니다.')
-        return redirect('accounts:mypage_2', pk)
+        messages.warning(request, "스스로 팔로우 할 수 없습니다.")
+        return redirect("accounts:mypage_2", pk)
 
     if request.user in user.followers.all():
         user.followers.remove(request.user)
     else:
         user.followers.add(request.user)
 
-    return redirect('accounts:mypage_2', pk)
+    return redirect("accounts:mypage_2", pk)
+
 
 def update(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('accounts:mypage_1', request.user.pk)
+            return redirect("accounts:mypage_1", request.user.pk)
     else:
         form = CustomUserChangeForm(instance=request.user)
-    
+
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'accounts/update.html', context)
+    return render(request, "accounts/update.html", context)
