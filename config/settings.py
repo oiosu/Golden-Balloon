018e35c0473df65acf54ed7dbabfd2cabdf44cb9
oiosu <kdlatnrud123@naver.com>
2022-11-06 19:23:05 +0900

    css 오류

--- config/settings.py
+++ config/settings.py
@@ -92,15 +92,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
@@ -124,7 +118,8 @@ USE_TZ = True
 import os.path
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join("static"),)
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = (os.path.join("static"),)
 
 # Media files (user uploaded filed)
 
