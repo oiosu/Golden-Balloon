20d77b8ab739363a89e4380bf7f9bbc20ac17a2d
ererink <yelkim0210@gmail.com>
2022-11-08 17:35:43 +0900

    fix heroku error

--- config/settings.py
+++ config/settings.py
@@ -123,10 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / "static"), 
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 # STATICFILES_DIRS = (os.path.join("static"),)
 
 # Media files (user uploaded filed)
